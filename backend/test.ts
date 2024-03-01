d4394a1f59920ecd5b4cfc02811f974fe5e757bf
Michael Pontus <m.pontus@gmail.com>
2024-03-01 19:32:09 +0000

    Test parallel charges

--- backend/test.ts
+++ backend/test.ts
@@ -1,6 +1,7 @@
 import { performance } from "perf_hooks";
 import supertest from "supertest";
 import { buildApp } from "./app";
+import expect from "expect";
 
 const app = supertest(buildApp());
 
@@ -15,8 +16,27 @@ async function basicLatencyTest() {
     console.log(`Latency: ${performance.now() - start} ms`);
 }
 
+async function parallelChargeTest() {
+    await app.post("/reset").expect(204);
+    const results = await Promise.all(
+        new Array(3).fill(0).map(() =>
+            app.post("/charge").send({
+                charges: 40,
+            }),
+        ),
+    );
+    expect(results.map((r) => r.body)).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({ isAuthorized: true, remainingBalance: 60 }),
+            expect.objectContaining({ isAuthorized: true, remainingBalance: 20 }),
+            expect.objectContaining({ isAuthorized: false, remainingBalance: 20 }),
+        ]),
+    );
+}
+
 async function runTests() {
     await basicLatencyTest();
+    await parallelChargeTest();
 }
 
 runTests().catch(console.error);
